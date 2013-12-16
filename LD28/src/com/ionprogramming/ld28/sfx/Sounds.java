000bb06c701622c8d3c62f780de141a3161e7179
Lucas Fraser <lucas@ionprogramming.com>
2013-12-16 18:08:03 +1030

    SFX

--- LD28/src/com/ionprogramming/ld28/sfx/Sounds.java
+++ LD28/src/com/ionprogramming/ld28/sfx/Sounds.java
@@ -10,7 +10,17 @@ public class Sounds {
 	static MediaPlayer music;
 	static ArrayList<MediaPlayer> players = new ArrayList<MediaPlayer>();
 
+	
+	
+	public static Media fire;
+	public static Media footsteps;
+	public static Media ghost1;
+	public static Media ghost2;
+	public static Media glass;
 	public static Media pit;
+	public static Media wind;
+
+	
 	public static Media s1;
 	public static Media s2;
 	public static Media s3;
@@ -19,12 +29,21 @@ public class Sounds {
 
 	public static void load(){
 		try{
+			fire = new Media(Sounds.class.getClassLoader().getResource("res/sfx/fire.mp3").toString());
+			footsteps = new Media(Sounds.class.getClassLoader().getResource("res/sfx/footsteps.mp3").toString());
+			ghost1 = new Media(Sounds.class.getClassLoader().getResource("res/sfx/ghost1.mp3").toString());
+			ghost2 = new Media(Sounds.class.getClassLoader().getResource("res/sfx/ghost2.mp3").toString());
+			glass = new Media(Sounds.class.getClassLoader().getResource("res/sfx/glass.mp3").toString());
 			pit = new Media(Sounds.class.getClassLoader().getResource("res/sfx/pit.mp3").toString());
+			wind = new Media(Sounds.class.getClassLoader().getResource("res/sfx/wind.mp3").toString());
+			
 			s1 = new Media(Sounds.class.getClassLoader().getResource("res/music/1.mp3").toString());
 			s2 = new Media(Sounds.class.getClassLoader().getResource("res/music/2.mp3").toString());
 			s3 = new Media(Sounds.class.getClassLoader().getResource("res/music/3.mp3").toString());
 			s4 = new Media(Sounds.class.getClassLoader().getResource("res/music/4.mp3").toString());
 			s5 = new Media(Sounds.class.getClassLoader().getResource("res/music/5.mp3").toString());
+
+
 		}
 		catch(Exception e){
 			e.printStackTrace();
